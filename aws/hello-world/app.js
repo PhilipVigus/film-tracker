242e425423330e5e314d0053a533e27bcc3782a4
PhilipVigus <philipvigus@yahoo.co.uk>
2021-01-04 08:44:41 +0000

    Add configuration for remote db

--- aws/hello-world/app.js
+++ aws/hello-world/app.js
@@ -19,10 +19,11 @@ let response;
 exports.lambdaHandler = async (event, context) => {
     const dbConfig = {
         "apiVersion": "2012-08-10",
-        "accessKeyId": "1234",
-        "secretAccessKey": "1234",
+        //"accessKeyId": "1234",
+        //"secretAccessKey": "1234",
         "region":"eu-west-2",
-        "endpoint": "http://film-tracker-local-dynamodb:8000"
+        //"endpoint": "http://film-tracker-local-dynamodb:8000",
+        //"endpoint": "https://arn:aws:dynamodb:eu-west-2:485675000042"
     };
 
     const testTableParams = {
@@ -45,7 +46,7 @@ exports.lambdaHandler = async (event, context) => {
     try {
         const db = new DynamoDB(dbConfig);
         await db.createTable(testTableParams).promise();
-        await db.deleteTable({ TableName: "testTable"}).promise();
+        //await db.deleteTable({ TableName: "testTable"}).promise();
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
